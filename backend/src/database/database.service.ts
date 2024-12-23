8bebc4990c16b1a83539dfc034da4ea8e229d172
Erthium <ertyum35@gmail.com>
2024-12-23 18:55:18 +0300

    feat(backend): create database service

---
+++ backend/src/database/database.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { Pool } from 'pg';
+
+@Injectable()
+export class DatabaseService {
+  private readonly pool: Pool;
+
+  constructor() {
+    this.pool = new Pool({
+      database: process.env.DB_NAME,
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      user: process.env.DB_NAME,
+      password: process.env.DB_PASSWORD
+    });
+  }
+
+  async query(query: string, params: any[] = []): Promise<any> {
+    const client = await this.pool.connect();
+    console.log(`log: running query '${query}' with params '${params}'`);
+    try {
+      return await client.query(query, params);
+    }
+    finally {
+      client.release();
+    }
+  }
+}
