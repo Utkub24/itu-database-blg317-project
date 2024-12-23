415a251a2db878ec5adf2cfe206c95db87b9eed2
Hüseyin Şimşek <huseyinsimsek7904@gmail.com>
2024-12-23 20:41:17 +0300

    feat: fix get pokemon query

--- database/queries/pokemon/get.sql
+++ database/queries/pokemon/get.sql
@@ -1,3 +1,1 @@
-select pokemon.pokemon_id, name, height, weight from pokemon join pokemon_moves
-on pokemon.pokemon_id = pokemon_moves.pokemon_id
-where pokemon_moves.move_id = $1;
+select * from pokemon where pokemon_moves.move_id = $1;
