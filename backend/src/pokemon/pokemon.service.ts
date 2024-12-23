85e79bfee61dd54720476074f2cc06db89f33699
Erthium <ertyum35@gmail.com>
2024-12-23 21:16:35 +0300

    feat(backend): add get pokemon by id queries

--- backend/src/pokemon/pokemon.service.ts
+++ backend/src/pokemon/pokemon.service.ts
@@ -12,8 +12,28 @@ export class PokemonService {
   }
 
   async getPokemonById(id: string) {
-    const query = '';
-    return await this.databaseService.query(query, [id]);
+    const pokemon_query = 'select * from pokemon where pokemon_id = $1;';
+    const pokemon = await this.databaseService.query(pokemon_query, [id]).then(res => res.rows);
+
+    const moves_query = 'select move.move_id, move.name from move join pokemon_moves on move.move_id = pokemon_moves.move_id where pokemon_moves.pokemon_id = $1;';
+    const moves = await this.databaseService.query(moves_query, [id]).then(res => res.rows);    
+
+    const abilities_query = 'select ability.ability_id, ability.name from ability join pokemon_abilities on ability.ability_id = pokemon_abilities.ability_id where pokemon_abilities.pokemon_id = $1;';
+    const abilities = await this.databaseService.query(abilities_query, [id]).then(res => res.rows);
+
+    const types_query = 'select type.type_id, type.type_name from type join pokemon_types on type.type_id = pokemon_types.type_id where pokemon_types.pokemon_id = $1;';
+    const types = await this.databaseService.query(types_query, [id]).then(res => res.rows);
+
+    const pokemonDto = new PokemonDto(
+      pokemon[0].id,
+      pokemon[0].name,
+      pokemon[0].weight,
+      pokemon[0].height,
+      types.map(type => new TypeDto(type.type_id, type.type_name)),
+      moves.map(move => new MoveDto(move.mode_id, move.name)),
+      abilities.map(ability => new AbilityDto(ability.ability_id, ability.name))
+    );
+    return pokemonDto;
   }
 
   async createPokemon() {
