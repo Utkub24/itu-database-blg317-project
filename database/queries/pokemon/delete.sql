bb3ddfabb8f99efd5f9b94c5c0e3370f1be6808d
Hüseyin Şimşek <huseyinsimsek7904@gmail.com>
2024-12-23 21:28:03 +0300

    fix(database): add delete clauses for all tables

--- database/queries/pokemon/delete.sql
+++ database/queries/pokemon/delete.sql
@@ -1,1 +1,4 @@
 delete from pokemon where pokemon_id = $1;
+delete from pokemon_types where pokemon_id = $1;
+delete from pokemon_moves where pokemon_id = $1;
+delete from pokemon_abilities where pokemon_id = $1;
