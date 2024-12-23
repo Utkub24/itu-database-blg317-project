a275aa882c3d586618f0215a27fb33a82b041be6
Hüseyin Şimşek <huseyinsimsek7904@gmail.com>
2024-12-23 21:14:19 +0300

    fix(backend): fix queries

--- database/queries/move/from_pokemon.sql
+++ database/queries/move/from_pokemon.sql
@@ -1,1 +1,1 @@
-select (type.move_id, type.name) from move join pokemon_moves on move.move_id = pokemon_moves.move_id where pokemon_moves.pokemon_id = $1;
+select type.move_id, type.name from move join pokemon_moves on move.move_id = pokemon_moves.move_id where pokemon_moves.pokemon_id = $1;
--- database/queries/type/from_pokemon.sql
+++ database/queries/type/from_pokemon.sql
@@ -1,1 +1,1 @@
-select (type.type_id, type.type_name) from type join pokemon_types on type.type_id = pokemon_types.type_id where pokemon_types.pokemon_id = $1;
+select type.type_id, type.type_name from type join pokemon_types on type.type_id = pokemon_types.type_id where pokemon_types.pokemon_id = $1;
