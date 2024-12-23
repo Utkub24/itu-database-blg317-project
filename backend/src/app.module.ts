e64fc0032eed7f307866636aaf04b38ead699840
Erthium <ertyum35@gmail.com>
2024-12-23 18:58:02 +0300

    feat(backemd): add new services and controllers to the app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PokemonService } from './pokemon/pokemon.service';
+import { PokemonController } from './pokemon/pokemon.controller';
+import { DatabaseService } from './database/database.service';
+import { AbilityService } from './ability/ability.service';
+import { AbilityController } from './ability/ability.controller';
+import { MoveService } from './move/move.service';
+import { MoveController } from './move/move.controller';
+import { TypeService } from './type/type.service';
+import { TypeController } from './type/type.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, PokemonController, AbilityController, MoveController, TypeController],
+  providers: [AppService, PokemonService, DatabaseService, AbilityService, MoveService, TypeService],
 })
 export class AppModule {}
